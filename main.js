287470559f2e5cbb9edfc45958e377d2e6f31722
Aliyah <aliyahrenomeron7@gmail.com>
2026-01-11 12:54:03 -0500

    Updated the width and height of the program

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 442,
-        height: 468
+        width: 270,
+        height: 330
     })
 
     win.loadFile('index.html');
